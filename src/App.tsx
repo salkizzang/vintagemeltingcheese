a4ab25297013853f8eb588408a06670a6dedb8ee
최원석 <salki@choewonseog-ui-Macmini.local>
2026-02-08 19:03:09 +0900

    fix: 모바일 인스타 이동 이벤트 수정, 이미지 링크 수정

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,25 @@ function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [showScrollTop, setShowScrollTop] = useState(false);
 
+  // 인스타그램 주소 상수화
+  const INSTAGRAM_URL = "https://www.instagram.com/melting_cheese_official/";
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleInstagramClick = (e : any) => {
+    e.preventDefault(); // 기본 링크 이동 막기
+    
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isMobile = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
+
+    if (isMobile) {
+      // 모바일: 현재 창에서 이동 (이렇게 해야 앱이 딥링크를 제대로 인식함)
+      window.location.href = INSTAGRAM_URL;
+    } else {
+      // PC: 새 탭으로 열기
+      window.open(INSTAGRAM_URL, '_blank');
+    }
+  };
+
   useEffect(() => {
     const handleScroll = () => {
       setShowScrollTop(window.scrollY > 400);
@@ -49,8 +68,8 @@ function App() {
 
       {/* Marquee */}
       <a
-        href="https://www.instagram.com/melting_cheese_official/"
-        target="_blank"
+        href={INSTAGRAM_URL}
+        onClick={handleInstagramClick}
         rel="noopener noreferrer"
         className="marquee-container"
         style={{ marginTop: '80px' }}
@@ -251,7 +270,9 @@ function App() {
         <div className="footer-bottom">
         <p>COMPANY: 멜팅치즈(Melting Cheese) | OWNER: Moa Choi</p>
         <p>E-MAIL min9030504@naver.com</p>
-        <a href="https://www.instagram.com/melting_cheese_official/" target="_blank" rel="noopener noreferrer" className="footer-instagram">
+        <a href={INSTAGRAM_URL}
+        onClick={handleInstagramClick}
+         rel="noopener noreferrer" className="footer-instagram">
           <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg>
           @melting_cheese_official
         </a>
--- src/data/products.ts
+++ src/data/products.ts
@@ -12,7 +12,7 @@ export const products: Product[] = [
     id: "12895131962",
     name: "말랑 위아래 인덱스형 북마커 책갈피",
     price: 1500,
-    image: "http://shop1.phinf.naver.net/20251229_168/1766974487780ih80W_PNG/37481454365834775_539070738.png",
+    image: "https://shop1.phinf.naver.net/20251229_168/1766974487780ih80W_PNG/37481454365834775_539070738.png",
     category: "독서용품",
     smartstoreUrl: "https://smartstore.naver.com/vintagemeltingcheese/products/12895131962"
   },
@@ -20,7 +20,7 @@ export const products: Product[] = [
     id: "12867462975",
     name: "포그니 B6 A5 다이어리 북 책 파우치",
     price: 16000,
-    image: "http://shop1.phinf.naver.net/20251223_130/1766456142817J7yxr_PNG/83599229588541416_572854869.png",
+    image: "https://shop1.phinf.naver.net/20251223_130/1766456142817J7yxr_PNG/83599229588541416_572854869.png",
     category: "다이어리",
     smartstoreUrl: "https://smartstore.naver.com/vintagemeltingcheese/products/12867462975"
   },
@@ -36,7 +36,7 @@ export const products: Product[] = [
     id: "12476421882",
     name: "빈티지플라워샵 B6 A5 다이어리커버",
     price: 16000,
-    image: "http://shop1.phinf.naver.net/20250929_281/1759113274726tabgl_JPEG/2427716736448742_1716282849.jpg",
+    image: "https://shop1.phinf.naver.net/20250929_281/1759113274726tabgl_JPEG/2427716736448742_1716282849.jpg",
     category: "다이어리",
     smartstoreUrl: "https://smartstore.naver.com/vintagemeltingcheese/products/12476421882"
   },
@@ -44,7 +44,7 @@ export const products: Product[] = [
     id: "12229700073",
     name: "퐁퐁 이너백 파우치 스트링파우치",
     price: 5000,
-    image: "http://shop1.phinf.naver.net/20250810_126/175483147383638dLT_JPEG/80860959072840186_1465207397.jpg",
+    image: "https://shop1.phinf.naver.net/20250810_126/175483147383638dLT_JPEG/80860959072840186_1465207397.jpg",
     category: "파우치",
     smartstoreUrl: "https://smartstore.naver.com/vintagemeltingcheese/products/12229700073"
   },
@@ -52,7 +52,7 @@ export const products: Product[] = [
     id: "12229633709",
     name: "핑크드림 미니백 에코백 파우치",
     price: 12000,
-    image: "http://shop1.phinf.naver.net/20250810_182/1754832056648IB0he_JPEG/26083476788222478_1110671517.jpg",
+    image: "https://shop1.phinf.naver.net/20250810_182/1754832056648IB0he_JPEG/26083476788222478_1110671517.jpg",
     category: "에코백",
     smartstoreUrl: "https://smartstore.naver.com/vintagemeltingcheese/products/12229633709"
   },
@@ -60,7 +60,7 @@ export const products: Product[] = [
     id: "12229589620",
     name: "퐁퐁 에코백 숄더백 데일리백",
     price: 19000,
-    image: "http://shop1.phinf.naver.net/20250810_289/1754829109115PiJN4_JPEG/17816776980950330_12219837.jpg",
+    image: "https://shop1.phinf.naver.net/20250810_289/1754829109115PiJN4_JPEG/17816776980950330_12219837.jpg",
     category: "에코백",
     smartstoreUrl: "https://smartstore.naver.com/vintagemeltingcheese/products/12229589620"
   },
@@ -68,7 +68,7 @@ export const products: Product[] = [
     id: "12035231476",
     name: "몽글 동전지갑 카드지갑 미니파우치",
     price: 9900,
-    image: "http://shop1.phinf.naver.net/20250629_69/1751204020408WH7kf_JPEG/85336948501767175_1920349175.jpg",
+    image: "https://shop1.phinf.naver.net/20250629_69/1751204020408WH7kf_JPEG/85336948501767175_1920349175.jpg",
     category: "지갑",
     smartstoreUrl: "https://smartstore.naver.com/vintagemeltingcheese/products/12035231476"
   },
@@ -84,7 +84,7 @@ export const products: Product[] = [
     id: "11412740579",
     name: "체리가조아 B6 A5 다이어리커버",
     price: 18000,
-    image: "http://shop1.phinf.naver.net/20250130_157/1738241615592MaBXn_JPEG/85206870385011873_720663754.jpg",
+    image: "https://shop1.phinf.naver.net/20250130_157/1738241615592MaBXn_JPEG/85206870385011873_720663754.jpg",
     category: "다이어리",
     smartstoreUrl: "https://smartstore.naver.com/vintagemeltingcheese/products/11412740579"
   },
@@ -92,7 +92,7 @@ export const products: Product[] = [
     id: "11412686518",
     name: "핑크드림 에코백 숄더백",
     price: 17000,
-    image: "http://shop1.phinf.naver.net/20250130_297/1738241487548SLdke_JPEG/72374420657792040_774035018.jpg",
+    image: "https://shop1.phinf.naver.net/20250130_297/1738241487548SLdke_JPEG/72374420657792040_774035018.jpg",
     category: "에코백",
     smartstoreUrl: "https://smartstore.naver.com/vintagemeltingcheese/products/11412686518"
   },
@@ -100,7 +100,7 @@ export const products: Product[] = [
     id: "11116583968",
     name: "오동통 빈티지 핸들 파우치",
     price: 8500,
-    image: "http://shop1.phinf.naver.net/20241106_257/1730855299163Fw9tR_JPEG/9474419869847728_1712755476.jpg",
+    image: "https://shop1.phinf.naver.net/20241106_257/1730855299163Fw9tR_JPEG/9474419869847728_1712755476.jpg",
     category: "파우치",
     smartstoreUrl: "https://smartstore.naver.com/vintagemeltingcheese/products/11116583968"
   }
